0842a61be9be5f09ac8af3209fbe0c2cee76959d
Zsofia Nemeth <zsofia.nemeth@upscale.hu>
2019-07-01 15:32:36 +0200

    basic login layout added

--- src/app/app-material/app-material.module.ts
+++ src/app/app-material/app-material.module.ts
@@ -1,25 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule } from '@angular/material';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { 
+         MatButtonModule, 
+         MatCardModule, 
+         MatFormFieldModule,
+         MatInputModule,
+         MatToolbarModule        
+        } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   imports: [
     CommonModule,
-    MatInputModule,
-    MatButtonModule,
+    FormsModule,
+    MatButtonModule, 
+    MatCardModule, 
     MatFormFieldModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatCardModule
+    MatInputModule,
+    MatToolbarModule
   ],
   exports: [
-    MatInputModule,
-    MatButtonModule,    
+    FormsModule,
+    MatButtonModule, 
+    MatCardModule, 
     MatFormFieldModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatCardModule
+    MatInputModule,
+    MatToolbarModule   
   ]
 })
 export class AppMaterialModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppMaterialModule } from './app-material/app-material.module';
 
-
-
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
