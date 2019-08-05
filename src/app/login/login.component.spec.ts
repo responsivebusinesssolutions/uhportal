8c725ecb2c68b5e23aa2bf6e066c9603b7a9b797
Krisztina Kiraly <krisztina.kiraly@rbsolutions.hu>
2019-08-05 07:07:15 +0200

    add unit tests to LoginComponent

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,30 +1,50 @@
+import { AuthenticationService } from './../_services/authentication.service';
 import { AppMaterialModule } from './../app-material/app-material.module';
 import { async, fakeAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { LoginComponent } from './login.component';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router, ActivatedRoute } from '@angular/router';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { By } from '@angular/platform-browser';
+import { of, throwError } from 'rxjs';
 
-describe('LoginComponent', () => {
+class AuthenticationServiceStub {
+  login = jasmine.createSpy('login');
+  currentUserValue = jasmine.createSpy('currentUserValue');
+}
+
+class RouterStub {
+  navigate = jasmine.createSpy('navigate');
+}
+
+fdescribe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
   let el: HTMLElement;
+  let authenticationService: AuthenticationServiceStub;
+  let router: Router;
+  let activatedRoute: ActivatedRoute;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ 
-        AppMaterialModule, 
+      imports: [
+        AppMaterialModule,
         BrowserAnimationsModule,
         HttpClientTestingModule,
-        RouterTestingModule
+        ReactiveFormsModule,
        ],
-      providers: [
-        FormBuilder
-      ],
       declarations: [ LoginComponent ],
+      providers: [
+        {provide: AuthenticationService, useClass: AuthenticationServiceStub},
+        {provide: Router, useClass: RouterStub},
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { queryParams: { returnUrl: 'home' } } },
+        },
+      ],
       schemas: [ NO_ERRORS_SCHEMA ]
 
     })
@@ -35,30 +55,89 @@ describe('LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    authenticationService = TestBed.get(AuthenticationService);
+    router = TestBed.get(Router);
+    activatedRoute = TestBed.get(ActivatedRoute);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Should set submitted to true', async(() => {
-    component.onSubmit();
-    expect(component.onSubmit).toBeTruthy();
-  }));
-  
-  it('Should call the OnSubmit method', () =>{ fakeAsync(() => {
-    fixture.detectChanges();
-    spyOn(component,'onSubmit');
-    el=fixture.debugElement.query(By.css('.login')).nativeElement;
-    el.click();
-    expect(component.onSubmit).toHaveBeenCalledTimes(1);
-  })
+  it('should navigate when currentUser is defined', () => {
+    authenticationService.currentUserValue.and.returnValue({});
+
+    expect(router.navigate).toHaveBeenCalledTimes(1);
+    expect(router.navigate).toHaveBeenCalledWith(['/']);
   });
 
-  it('Form should be invalid', async(() => {
-    component.loginForm.controls['username'].setValue('');
-    component.loginForm.controls['password'].setValue('');
-    expect(component.loginForm.valid).toBeFalsy();
-  }));
+  describe('onInit', () => {
+    it('should create loginForm', () => {
+      expect(component.loginForm).toBeDefined();
+      expect(component.loginForm.value).toEqual({
+        username: '',
+        password: ''
+      });
+    });
 
+    it('should set returnUrl according to queryparam', () => {
+      expect(component.returnUrl).toEqual('home');
+    });
+  });
+
+  describe('onSubmit', () => {
+    it('should be called when form is submitted', () => {
+      fixture.detectChanges();
+      spyOn(component, 'onSubmit');
+      el = fixture.debugElement.query(By.css('.login')).nativeElement;
+
+      el.click();
+
+      expect(component.onSubmit).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call authenticationService with form values', () => {
+      const username = 'user';
+      const password = 'password';
+
+      component.loginForm.patchValue({
+        username,
+        password
+      });
+      authenticationService.login.and.returnValue(of({}));
+
+      component.onSubmit();
+
+      expect(authenticationService.login).toHaveBeenCalledTimes(1);
+      expect(authenticationService.login).toHaveBeenCalledWith(username, password);
+    });
+
+    it('should call router navigate when authenticationService responds without error', () => {
+      authenticationService.login.and.returnValue(of({}));
+      const expectedUrl = '/';
+
+      component.onSubmit();
+
+      expect(router.navigate).toHaveBeenCalledTimes(1);
+      expect(router.navigate).toHaveBeenCalledWith([expectedUrl]);
+    });
+
+    it('should not call authenticationService when form is invalid', () => {
+      authenticationService.login.and.returnValue(of({}));
+      component.loginForm.patchValue({
+        username: 'test'
+      });
+
+      component.onSubmit();
+
+      expect(authenticationService.login).not.toHaveBeenCalled();
+    });
+
+    it('should set success to null', () => {
+      component.onSubmit();
+
+      expect(component.success).toBeNull();
+    });
+  });
 });
