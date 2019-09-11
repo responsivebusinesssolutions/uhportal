a0ba47767982ff81dce9b8cac3ff8a102ac2b141
Zsofia Nemeth <zsofia.nemeth@upscale.hu>
2019-09-11 13:34:07 +0200

    app component logic moved to home component

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,4 +1,8 @@
+import { AuthenticationService } from './../../services/authentication.service';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { User } from './../../interfaces/user';
+
 
 @Component({
   selector: 'app-home',
@@ -6,11 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  currentUser: User;
+  title = 'uhportal';
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService) {
+      this.authenticationService.currentUser.subscribe(currentUser => this.currentUser = currentUser);
+     }
 
   ngOnInit() {
   }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+}
 
 }
 
