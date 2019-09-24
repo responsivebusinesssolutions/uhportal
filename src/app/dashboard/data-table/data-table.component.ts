ff62edf4828cee3ba0ca64a4cba7591e9e6f0709
Andras Barabas <andrasbarabas@Andrass-MacBook-Pro.local>
2019-09-24 13:34:11 +0200

    update angular to 9

--- src/app/dashboard/data-table/data-table.component.ts
+++ src/app/dashboard/data-table/data-table.component.ts
@@ -10,9 +10,9 @@ import { MatTableDataSource } from '@angular/material';
   styleUrls: ['./data-table.component.scss']
 })
 export class DataTableComponent implements AfterViewInit, OnInit {
-  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: false}) sort: MatSort;
-  @ViewChild(MatTable, {static: false}) table: MatTable<DataTableItem>;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatTable) table: MatTable<DataTableItem>;
   dataSource: MatTableDataSource<DataTableItem>;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
