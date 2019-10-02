e30a51b834d7669a223245336b064c52a214d986
Zsofia Nemeth <zsofia.nemeth@upscale.hu>
2019-10-02 10:30:58 +0200

    fixed test setup and added register and login component tests

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,15 +1,12 @@
+import { LoginInput } from './../models/login-input.model';
 import { MaterialModule } from './../../shared/material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { DebugElement } from '@angular/core';
-import { RouterTestingModule } from '@angular/router/testing';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login.component';
 import { AuthService } from './../auth.service';
-import { AuthModule } from './../auth.module';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { By } from '@angular/platform-browser';
 import { of, throwError } from 'rxjs';
@@ -32,23 +29,11 @@ describe('LoginComponent', () => {
   let activatedRoute: ActivatedRoute;
 
   beforeEach(async(() => {
-    const authServiceSpy = jasmine.createSpy('AuthService');
-
     TestBed.configureTestingModule({
+      imports: [MaterialModule, BrowserAnimationsModule, HttpClientTestingModule, ReactiveFormsModule],
       declarations: [LoginComponent],
-      imports: [
-        AuthModule,
-        BrowserAnimationsModule,
-        FormsModule,
-        HttpClientTestingModule,
-        MaterialModule,
-        NoopAnimationsModule,
-        ReactiveFormsModule,
-        RouterTestingModule
-      ],
       providers: [
-        FormBuilder,
-        { provide: AuthService, useValue: authServiceSpy },
+        { provide: AuthService, useClass: AuthenticationServiceStub },
         { provide: Router, useClass: RouterStub },
         {
           provide: ActivatedRoute,
@@ -61,12 +46,22 @@ describe('LoginComponent', () => {
       .then(() => {
         fixture = TestBed.createComponent(LoginComponent);
         component = fixture.componentInstance;
-        activatedRoute = TestBed.get(ActivatedRoute);
+        activatedRoute = TestBed.inject(ActivatedRoute);
         fixture.detectChanges();
         authService = TestBed.get(AuthService);
-        router = TestBed.get(Router);
+        router = TestBed.inject(Router);
       });
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    authService = TestBed.get(AuthService);
+    router = TestBed.inject(Router);
+    activatedRoute = TestBed.inject(ActivatedRoute);
+  });
 
   it('should compile', () => {
     expect(component).toBeTruthy();
@@ -89,16 +84,21 @@ describe('LoginComponent', () => {
     expect(component.onSubmit).toHaveBeenCalledTimes(1);
   });
 
-  it('should call authenticationService with form values', async(() => {
-    // const username = 'user';
-    // const password = 'password';
-    // component.loginForm.patchValue({
-    //   username,
-    //   password
-    // });
-    // authService.login.and.returnValue(of({}));
-    // component.onSubmit();
-    // expect(authService.login).toHaveBeenCalledTimes(1);
-    // expect(authService.login).toHaveBeenCalledWith(username, password);
-  }));
+  it('should call authenticationService with form values', () => {
+    const username = 'username';
+    const password = 'password';
+    const loginInput: LoginInput = { username: 'username', password: 'password' };
+
+    component.loginForm.patchValue({
+      username,
+      password
+    });
+
+    authService.login.and.returnValue(of({}));
+
+    component.onSubmit();
+
+    expect(authService.login).toHaveBeenCalledTimes(1);
+    expect(authService.login).toHaveBeenCalledWith(new LoginInput(username, password));
+  });
 });
--- src/app/auth/register/register.component.spec.ts
+++ src/app/auth/register/register.component.spec.ts
@@ -1,41 +1,83 @@
+import { AuthService } from './../auth.service';
 import { MaterialModule } from './../../shared/material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { MatPaginatorModule, MatSortModule, MatTableModule } from '@angular/material';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { RouterTestingModule } from '@angular/router/testing';
 import { RegisterComponent } from './register.component';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+
+class AuthenticationServiceStub {
+  register = jasmine.createSpy('register');
+  currentUserValue = jasmine.createSpy('currentUserValue');
+}
+
+class RouterStub {
+  navigate = jasmine.createSpy('navigate');
+}
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let authService: AuthenticationServiceStub;
+  let router: Router;
+  let activatedRoute: ActivatedRoute;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [RegisterComponent],
-      imports: [
-        BrowserAnimationsModule,
-        HttpClientTestingModule,
-        MatPaginatorModule,
-        MatSortModule,
-        MatTableModule,
-        MaterialModule,
-        RouterTestingModule
+      imports: [MaterialModule, BrowserAnimationsModule, HttpClientTestingModule, ReactiveFormsModule],
+      providers: [
+        { provide: AuthService, useClass: AuthenticationServiceStub },
+        { provide: Router, useClass: RouterStub },
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { queryParams: { returnUrl: 'home' } } }
+        }
       ],
-      providers: [FormBuilder],
       schemas: [NO_ERRORS_SCHEMA]
-    }).compileComponents();
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(RegisterComponent);
+        component = fixture.componentInstance;
+        activatedRoute = TestBed.inject(ActivatedRoute);
+        fixture.detectChanges();
+        authService = TestBed.get(AuthService);
+        router = TestBed.inject(Router);
+      });
   }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RegisterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
   it('should compile', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form invalid when empty', () => {
+    expect(component.registerForm.valid).toBeFalsy();
+  });
+
+  it('should call authenticationService with form values', () => {
+    const firstName = 'test';
+    const lastName = 'test';
+    const username = 'username';
+    const password = 'password';
+
+    component.registerForm.patchValue({
+      firstName,
+      lastName,
+      username,
+      password
+    });
+
+    authService.register.and.returnValue(of({}));
+
+    component.onSubmit();
+
+    expect(authService.register).toHaveBeenCalledTimes(1);
+    expect(authService.register).toHaveBeenCalledWith(
+      Object({ firstName: 'test', lastName: 'test', username: 'username', password: 'password' })
+    );
   });
 });
